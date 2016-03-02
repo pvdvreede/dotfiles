d8d128cee765470767567a5c10b95c570ab695ff
pvdvreede <pvdvreede@gmail.com>
2016-03-02 14:15:00 +1100

    Add basic fish config with aliases.

---
+++ config.fish
@@ -0,0 +1,14 @@
+# cli
+alias 'll' 'ls -lah'
+
+# git
+alias 'gc' 'git commit -v'
+alias 'ga' 'git add'
+alias 'gst' 'git status'
+alias 'gco' 'git checkout'
+alias 'gd' 'git diff'
+alias 'gdc' 'git diff --cached'
+alias 'glol' "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+# docker
+alias 'fig' 'docker-compose'

1bed7bc52b58b66700498ed3af1aa469d0dfc22d
pvdvreede <pvdvreede@gmail.com>
2016-03-23 11:30:07 +1100

    Update shell configs for bash and fish.

--- bashrc
+++ bashrc
@@ -43,7 +43,6 @@ alias gdc="git diff --cached"
 alias glol="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias gpr="git pull --rebase"
 
-alias ls="ls --color"
 alias ll="ls -lah"
 alias ..="cd .."
 alias ...="cd ../../"
--- config.fish
+++ config.fish
@@ -1,17 +1,60 @@
+function fish_prompt
+  printf '%s%s%s >%s ' (set_color cyan) (prompt_pwd) (set_color white) (set_color normal)
+end
+
+function fish_greeting
+end
+
 # cli
-alias 'll' 'ls -lah'
+abbr 'll' 'ls -lah'
 
 # git
-alias 'gc' 'git commit -v'
-alias 'ga' 'git add'
-alias 'gst' 'git status'
-alias 'gco' 'git checkout'
-alias 'gd' 'git diff'
-alias 'gdc' 'git diff --cached'
-alias 'glol' "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+abbr 'gc' 'git commit -v'
+abbr 'gb' 'git branch'
+abbr 'ga' 'git add'
+abbr 'gst' 'git status'
+abbr 'gco' 'git checkout'
+abbr 'gd' 'git diff'
+abbr 'gdc' 'git diff --cached'
+abbr 'glol' "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+# tmux
+alias 'tn' 'tmux new-session -A -s '
+alias 'tna' 'tmux new-session -A -s (basename $PWD | tr . -)'
+alias 'tl' 'tmux ls'
 
 # docker
 alias 'fig' 'docker-compose'
+
+set -U EDITOR (which vim)
+
+# docker based functions
+function run-docker
+  if test (count $argv) -gt 2
+    set cmd $argv[3..-1]
+  end
+  docker run -t -i --rm -v (pwd):/app -w /app --entrypoint $argv[1] $argv[2] $cmd
+end
+
+function mix
+  run-docker mix msaraiva/elixir:1.2.2 $argv
+end
+
+function iex
+  run-docker iex msaraiva/elixir:1.2.2 $argv
+end
+
+function irb
+  run-docker irb ruby:2.3
+end
+
+function ruby
+  run-docker ruby ruby:2.3
+end
+
+function lein
+  run-docker lein clojure:latest
+end
 
 if test -f $HOME/.custom.fish
   source $HOME/.custom.fish

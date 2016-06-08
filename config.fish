955b5b08905f0e78b6da68a6a564b24c5dd8d0b7
pvdvreede <pvdvreede@gmail.com>
2016-06-08 11:51:36 +1000

    Add export function to fish for bash compatability.

--- config.fish
+++ config.fish
@@ -5,6 +5,16 @@ end
 function fish_greeting
 end
 
+function export
+  if [ $argv ]
+    set var (echo $argv | cut -f1 -d=)
+    set val (echo $argv | cut -f2 -d=)
+    set -g -x $var $val
+  else
+    echo 'export var=value'
+  end
+end
+
 # cli
 abbr 'll' 'ls -lah'
 

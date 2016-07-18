9bf8b9eedebe44ba04bc12b44105ea85ccc1edcb
pvdvreede <pvdvreede@gmail.com>
2016-07-18 10:58:34 +1000

    Add line numbers in normal state to nakemacs.

--- layers/nakemacs/init.el
+++ layers/nakemacs/init.el
@@ -6,20 +6,30 @@
 (setq visible-bell nil)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(menu-bar-mode -1)		      
+(menu-bar-mode -1)
 
 (set-frame-font "Source Code Pro 12")
 
 (setq-default mode-line-format nil)
 
-(set-fringe-mode
- (/ (- (frame-pixel-width)
-       (* 120 (frame-char-width))) 2))
+(defun pv/line-numbers-on ()
+  (interactive)
+  (set-fringe-mode (* 2 (frame-char-width)))
+  (setq linum-format "  %4d")
+  (linum-mode 1))
+
+(defun pv/line-numbers-off ()
+  (interactive)
+  (set-fringe-mode (* 8 (frame-char-width)))
+  (linum-mode -1))
+
+(setq require-final-newline t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (set-face-attribute
-  'fringe nil
-  :foreground (face-foreground 'default)
-  :background (face-background 'default))
+ 'fringe nil
+ :foreground (face-foreground 'default)
+ :background (face-background 'default))
 
 (setq pop-up-windows nil)
 (setq x-select-enable-clipboard t)
@@ -40,41 +50,66 @@
   (package-install 'use-package))
 
 (use-package yaml-mode
-	     :ensure t)
+  :ensure t)
 
 (use-package dockerfile-mode
-	     :ensure t)
+  :ensure t)
 
 (use-package atom-one-dark-theme
-	     :ensure t
-	     :config
-	     (load-theme 'atom-one-dark t))
+  :ensure t
+  :config
+  (load-theme 'atom-one-dark t))
 
 (use-package helm
-	     :commands
-	     helm-M-x
-	     helm-mini
-	     :ensure t
-	     :bind
-	     (("M-x" . helm-M-x)
-	      ("C-p" . helm-mini)
-	      :map helm-map
-	      ("<escape>" . keyboard-escape-quit))
-	     :config
-	     (progn
-	       (defadvice helm-display-mode-line
-		   (after undisplay-header activate)
-		 (setq mode-line-format nil)
-		 (setq header-line-format nil))))
+  :commands
+  helm-M-x
+  helm-mini
+  helm-find-files
+  :ensure t
+  :bind
+  (("M-x" . helm-M-x)
+   :map helm-map
+   ("<escape>" . keyboard-escape-quit))
+  :config
+  (progn
+    (defadvice helm-display-mode-line
+	(after undisplay-header activate)
+      (setq mode-line-format nil)
+      (setq header-line-format nil))))
 
 (use-package evil
-	     :ensure t
-	     :init
-	     (evil-mode 1)
-	     :bind
-	     (:map evil-normal-state-map
-		   (";" . evil-ex)))
-	     
+  :ensure t
+  :init
+  (evil-mode 1)
+  :config
+  (setq evil-move-cursor-back nil)
+  :bind
+  (:map evil-normal-state-map
+	("C-p" . helm-mini)
+	("C-u" . evil-scroll-up)
+	(";" . evil-ex)))
 
+(use-package linum-relative
+  :ensure t
+  :config
+  (linum-relative-on))
 
+(pv/line-numbers-on)
 
+(add-hook
+ 'evil-insert-state-entry-hook
+ (lambda ()
+   (pv/line-numbers-off)
+   (set-cursor-color "green")))
+
+(add-hook
+ 'evil-normal-state-entry-hook
+ (lambda ()
+   (pv/line-numbers-on)
+   (set-cursor-color "cyan")))
+
+(add-hook
+ 'evil-visual-state-entry-hook
+ (lambda ()
+   (pv/line-numbers-on)
+   (set-cursor-color "yellow")))
